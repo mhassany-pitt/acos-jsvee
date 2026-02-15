70a79486ce778647905d4334f0ee163e33bc491b
Mohammad Hassany <moh70@pitt.edu>
2026-02-15 18:50:04 -0500

    add resizeIframe() for SPLICE protocol

--- index.js
+++ index.js
@@ -1,53 +1,104 @@
-var fs = require('fs');
-var htmlencode = require('htmlencode').htmlEncode;
+var fs = require("fs");
+var htmlencode = require("htmlencode").htmlEncode;
 
-var JSVEE = function() {};
+var JSVEE = function () {};
 
-JSVEE.addToHead = function(params) {
-  return '<link href="/static/jsvee/jsvee.css" rel="stylesheet">\n' +
+JSVEE.addToHead = function (params) {
+  return (
+    '<link href="/static/jsvee/jsvee.css" rel="stylesheet">\n' +
     '<script src="/static/jsvee/jquery.min.js" type="text/javascript"></script>\n' +
-    '<script src="/static/jsvee/JSVEE-min.js" type="text/javascript"></script>\n';
+    '<script src="/static/jsvee/JSVEE-min.js" type="text/javascript"></script>\n'
+  );
 };
 
-JSVEE.addToBody = function(params, handlers, req) {
-
-  var startLine = '';
+JSVEE.addToBody = function (params, handlers, req) {
+  var startLine = "";
   if (req.query.startline) {
-    startLine = ' data-jsvee-startline="' + htmlencode(req.query.startline) + '"';
+    startLine =
+      ' data-jsvee-startline="' + htmlencode(req.query.startline) + '"';
   }
 
-  var kelmu = '';
+  var kelmu = "";
   if (handlers.libraries.kelmu) {
-    kelmu = ' kelmu';
+    kelmu = " kelmu";
   }
 
-  return '<div class="jsvee-animation' + kelmu + '" data-id="' + htmlencode(params.name) + '"' + startLine + '></div>';
+  return (
+    '<div class="jsvee-animation' +
+    kelmu +
+    '" data-id="' +
+    htmlencode(params.name) +
+    '"' +
+    startLine +
+    "></div>" +
+    `
+  
+    <script>
+      const resizeIframe = () => {
+        parent.postMessage({
+          messageType: "resize",
+          iframeHeight: document.body.scrollHeight + 50,
+          iframeUrl: window.location.href
+        }, "*");
 
+        parent.postMessage({
+          subject: "lti.frameResize",
+          message_id: Math.random().toString(36).substring(2),
+          height: document.body.scrollHeight + 50,
+          width: document.body.scrollWidth + 50,
+        }, "*");
+      };
+      $(document).ready(function() {
+        resizeIframe();
+      });
+    </script>
+  `
+  );
 };
 
-JSVEE.initialize = function(req, params, handlers, cb) {
-
+JSVEE.initialize = function (req, params, handlers, cb) {
   // Initialize the content type
   params.headContent += JSVEE.addToHead(params);
   params.bodyContent += JSVEE.addToBody(params, handlers, req);
 
   // Initialize the content package
-  handlers.contentPackages[req.params.contentPackage].initialize(req, params, handlers, function() {
-    if (handlers.libraries.kelmu) {
-      handlers.libraries.kelmu.initialize(req, params, handlers, cb);
-    } else {
-      cb();
-    }
-  });
+  handlers.contentPackages[req.params.contentPackage].initialize(
+    req,
+    params,
+    handlers,
+    function () {
+      if (handlers.libraries.kelmu) {
+        handlers.libraries.kelmu.initialize(req, params, handlers, cb);
+      } else {
+        cb();
+      }
+    },
+  );
 };
 
-JSVEE.handleEvent = function(event, payload, req, res, protocolPayload, responseObj, cb) {
-  if (event == 'log') {
-    var dir = JSVEE.config.logDirectory + '/jsvee/' + req.params.contentPackage;
-    fs.mkdir(dir, 0775, function(err) {
-      var name = payload.animationId.replace(/\.|\/|\\|~/g, "-") + '.log';
-      var data = new Date().toISOString() + ' ' + payload.logId + ' ' + JSON.stringify(payload.log) + ' ' + JSON.stringify(protocolPayload || {}) + '\n';
-      fs.writeFile(dir + '/' + name, data, { flag: 'a' }, function(err) {
+JSVEE.handleEvent = function (
+  event,
+  payload,
+  req,
+  res,
+  protocolPayload,
+  responseObj,
+  cb,
+) {
+  if (event == "log") {
+    var dir = JSVEE.config.logDirectory + "/jsvee/" + req.params.contentPackage;
+    fs.mkdir(dir, 0775, function (err) {
+      var name = payload.animationId.replace(/\.|\/|\\|~/g, "-") + ".log";
+      var data =
+        new Date().toISOString() +
+        " " +
+        payload.logId +
+        " " +
+        JSON.stringify(payload.log) +
+        " " +
+        JSON.stringify(protocolPayload || {}) +
+        "\n";
+      fs.writeFile(dir + "/" + name, data, { flag: "a" }, function (err) {
         cb(event, payload, req, res, protocolPayload, responseObj);
       });
     });
@@ -56,24 +107,24 @@ JSVEE.handleEvent = function(event, payload, req, res, protocolPayload, response
   }
 };
 
-JSVEE.register = function(handlers, app, conf) {
+JSVEE.register = function (handlers, app, conf) {
   handlers.contentTypes.jsvee = JSVEE;
-  fs.mkdir(conf.logDirectory + '/jsvee', 0775, function(err) {});
+  fs.mkdir(conf.logDirectory + "/jsvee", 0775, function (err) {});
   JSVEE.config = conf;
 };
 
-JSVEE.namespace = 'jsvee';
+JSVEE.namespace = "jsvee";
 JSVEE.installedContentPackages = [];
-JSVEE.packageType = 'content-type';
+JSVEE.packageType = "content-type";
 
 JSVEE.meta = {
-  'name': 'jsvee',
-  'shortDescription': 'Content type for JSVEE program execution visualizations.',
-  'description': '',
-  'author': 'Teemu Sirkiä',
-  'license': 'MIT',
-  'version': '0.2.1',
-  'url': ''
+  name: "jsvee",
+  shortDescription: "Content type for JSVEE program execution visualizations.",
+  description: "",
+  author: "Teemu Sirkiä",
+  license: "MIT",
+  version: "0.2.1",
+  url: "",
 };
 
 module.exports = JSVEE;
